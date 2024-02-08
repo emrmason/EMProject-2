39b564f8f637e8d22813ee6ee51305bdd7558359
emrmason <mas20021@byui.edu>
2024-02-08 12:34:07 -0800

    Actors list and single actor working

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,11 +1,23 @@
 const mongodb = require("../connectDB/connection");
 const ObjectId = require("mongodb").ObjectId;
 
-const listMovies = async (req, res, next) => {
+// Resource: ChatGPT helped me with details for breaking down my code into reusable parts. YAY!!!
+const getCollection = async (base, collectionName) => {
+  const client = await mongodb.connectDB();
+  return client.db(base).collection(collectionName);
+};
+
+const getQuery = async (collection, query) => {
+  return await collection.find(query).toArray();
+};
+
+const listFilms = async (req, res, next) => {
   try {
-    const client = await mongodb.connectDB();
-    const collection = client.db("movies").collection("films");
-    const result = await collection.find().toArray();
+    // const client = await mongodb.connectDB();
+    // const collection = client.db("movies").collection("films");
+    const collection = await getCollection("movies", "films");
+    // const result = await collection.find().toArray();
+    const result = await getQuery(collection, {});
     if (result.length > 0) {
       res.send(result);
     } else {
@@ -17,12 +29,13 @@ const listMovies = async (req, res, next) => {
   }
 };
 
-const oneMovie = async (req, res, next) => {
+const oneFilm = async (req, res, next) => {
   try {
-    const movieID = new ObjectId(req.params.id);
-    const client = await mongodb.connectDB();
-    const collection = client.db("movies").collection("films");
-    const result = await collection.find({ _id: movieID }).toArray();
+    const movieId = new ObjectId(req.params.id);
+    // const client = await mongodb.connectDB();
+    const collection = await getCollection("movies", "films");
+    // const result = await collection.find({ _id: movieID }).toArray();
+    const result = await getQuery(collection, { _id: movieId });
     if (result.length > 0) {
       res.send(result);
     } else {
@@ -34,7 +47,40 @@ const oneMovie = async (req, res, next) => {
   }
 };
 
+const listActors = async (req, res, next) => {
+  try {
+    const collection = await getCollection("movies", "actors");
+    const result = await getQuery(collection, {});
+    if (result.length > 0) {
+      res.send(result);
+    } else {
+      res.send("Couldn't find anything...");
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
+const oneActor = async (req, res, next) => {
+  try {
+    const actorId = new ObjectId(req.params.id);
+    const collection = await getCollection("movies", "actors");
+    const result = await getQuery(collection, { _id: actorId });
+    if (result.length > 0) {
+      res.send(result);
+    } else {
+      console.log("Couldn't find the actor you're looking for.");
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+};
+
 module.exports = {
-  listMovies,
-  oneMovie,
+  listFilms,
+  oneFilm,
+  listActors,
+  oneActor,
 };
