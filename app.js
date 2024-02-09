7fd93afb912ff8d6037006c09a8c5b2d1d163ff2
emrmason <mas20021@byui.edu>
2024-02-09 09:40:24 -0800

    Swagger added.. PLEASE WORK.

--- app.js
+++ app.js
@@ -3,7 +3,10 @@ const app = express();
 const port = process.env.port || 3000;
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger-output.json");
 
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 require("dotenv").config();
 
 app.use(bodyParser.json());
--- routes/movies.js
+++ routes/movies.js
@@ -1,7 +1,7 @@
 const routes = require("express").Router();
 const moviesController = require("../controllers/movies");
 
-routes.get("/films", moviesController.listFilms);
+routes.get("/", moviesController.listFilms);
 routes.get("/oneFilm/:id", moviesController.oneFilm);
 routes.get("/actors", moviesController.listActors);
 routes.get("/oneActor/:id", moviesController.oneActor);
