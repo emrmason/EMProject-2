7d0661fccb13fb30f3f84bf454349e36f9034160
emrmason <mas20021@byui.edu>
2024-02-22 12:12:41 -0800

    Start of 0auth addition

--- app.js
+++ app.js
@@ -7,7 +7,24 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerDocument = require("./swagger-output.json");
 const dotenv = require("dotenv").config();
 const validator = require("./validator");
+const { auth } = require("express-openid-connect");
 
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: "a long, randomly-generated string stored in env",
+  baseURL: "http://localhost:3000",
+  clientID: "wTrjX8pkepNuUguHWYZebodjtdbc6g9s",
+  issuerBaseURL: "https://dev-ejq6nyomxig7wwih.us.auth0.com",
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
 // Swagger documentation
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
