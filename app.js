e026fa439adcbf6a5ba2a94342453ab2bc6f1479
emrmason <mas20021@byui.edu>
2024-02-23 11:07:51 -0800

    Auth0 working for SOME things.

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const config = {
   authRequired: false,
   auth0Logout: true,
   secret: process.env.AUTH0_CLIENT_SECRET,
-  baseURL: process.env.BASE_URL || "http://localhost:3000",
+  baseURL: "http://localhost:3000",
   clientID: process.env.AUTH0_CLIENT_ID,
   issuerBaseURL: process.env.AUTH0_ISSUER_BASE_URL,
 };
@@ -24,6 +24,9 @@ app.use(auth(config));
 // req.isAuthenticated is provided from the auth router
 app.get("/", (req, res) => {
   res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
 });
 
 // Swagger documentation
